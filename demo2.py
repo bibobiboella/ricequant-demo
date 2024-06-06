a7734044acc7671554a0066805388c65a0b83fc2
Ella Yan <81729699+bibobiboella@users.noreply.github.com>
2024-06-06 02:38:51 -0400

    修复: 1.每日动态行业分类数据 2.逐日回归

--- demo2.py
+++ demo2.py
@@ -6,7 +6,6 @@ import statsmodels.api as sm
 from scipy.stats import spearmanr
 rqdatac.init()
 
-
 start = '20240103'
 end = '20240601'
 period = 20
@@ -18,8 +17,6 @@ new_start = new_start_date.strftime('%Y%m%d')
 new_end = new_end_date.strftime('%Y%m%d')
 factor =  'pe_ratio'
 ticker = rqdatac.all_instruments('CS').order_book_id
-
-
 dates = pd.date_range(start=start, end=end, freq='B') 
 industry_data = pd.DataFrame()
 
@@ -30,7 +27,7 @@ for date in dates:
     industry_data = pd.concat([industry_data, industry_info])
 
 industry_data.reset_index(drop=True, inplace=True)
-print("动态?industry data: \n", industry_data)
+#print("动态 industry data: \n", industry_data)
 
 
 #因子
@@ -43,32 +40,50 @@ nan_columns = unstack_df.isna().any()
 df_sw_ind = rqdatac.shenwan_instrument_industry(ticker, level=1)
 #股票总市值
 market_cap_data = rqdatac.get_factor(ticker, 'market_cap', start_date, end_date)
-market_cap_df = pd.DataFrame(market_cap_data)
-
+market_cap_df = pd.DataFrame(market_cap_data).reset_index()
 #log(市值)
 market_cap_df['log_market_cap'] = np.log(market_cap_df['market_cap'])
 industry_dummies = pd.get_dummies(df_sw_ind['index_name'], prefix='industry')
+df = df.reset_index()
 
-#merge因子值/市值/行业哑变量
-factor_df = unstack_df.stack().reset_index()
-factor_df.columns = ['date', 'order_book_id', 'factor_value']
-market_cap_df = market_cap_df.reset_index()
-merged_df = factor_df.merge(market_cap_df[['order_book_id', 'date', 'log_market_cap']], on=['order_book_id', 'date'])#merge行
-merged_df = merged_df.merge(industry_dummies.reset_index(), on='order_book_id', how='left')#merge列
-merged_df = merged_df.replace([np.inf, -np.inf], np.nan).dropna()#避免inf
-exclude_columns = ['date', 'order_book_id', 'factor_value', 'log_market_cap']
-bool_columns = [col for col in merged_df.select_dtypes(include='bool').columns if col not in exclude_columns]
-merged_df[bool_columns] = merged_df[bool_columns].astype(int)
-merged_df = merged_df.set_index(['date', 'order_book_id']).dropna()
 
-#分X和y
-y = merged_df.pop("factor_value")
-X = merged_df.copy()
-X = sm.add_constant(X) 
-#print('X: \n', X)
-#print('y: \n', y)
-#线性回归
-model = sm.OLS(y, X.astype(float)).fit()
-#残差 = 中性化后的因子值
-print(model.resid)
+#result df
+neutralized_factor_data = pd.DataFrame()
+# 逐日回归
+for date in unstack_df.index:
+    date_str = date.strftime('%Y-%m-%d')
+    #因子值
+    factor_data_daily = df[df['date'] == date_str].reset_index()
+    #市值数据
+    market_cap_daily = market_cap_df[market_cap_df['date'] == date_str].reset_index()
+    #动态行业分类数据
+    industry_daily = industry_data[industry_data['date'] == date_str].reset_index()
+    #合并
+    merged_df = factor_data_daily.merge(market_cap_daily[['order_book_id', 'date', 'log_market_cap']], on=['order_book_id', 'date'])#merge行
+    merged_df = merged_df.merge(industry_dummies.reset_index(), on='order_book_id', how='left').drop('index', axis=1).dropna()#merge列
+    merged_df = merged_df.replace([np.inf, -np.inf], np.nan).dropna()#避免inf
+    exclude_columns = ['date', 'order_book_id', 'log_market_cap', 'pe_ratio']
+    bool_columns = [col for col in merged_df.select_dtypes(include='bool').columns if col not in exclude_columns]
+    merged_df[bool_columns] = merged_df[bool_columns].astype(int)
+    merged_df = merged_df.set_index(['date', 'order_book_id']).dropna()
+    #分X和y
+    y = merged_df.pop(factor)
+    X = merged_df.copy()
+    X = sm.add_constant(X) 
+    #print('X: \n', X)
+    #print('y: \n', y)
+    #线性回归
+    model = sm.OLS(y, X.astype(float)).fit()
+    #get残差
+    merged_df['neutralized_factor_value'] = model.resid
+    merged_df['date_'] = date_str
+    merged_df = merged_df.reset_index() #reset一下方便后面加date columns
+    #save result to a new df
+    neutralized_factor_data = pd.concat([neutralized_factor_data, merged_df[['date_', 'order_book_id', 'neutralized_factor_value']]])
+
+
+neutralized_factor_data.reset_index(drop=True, inplace=True)
+neutralized_factor_data = neutralized_factor_data.set_index(['date_', 'order_book_id'])
+# 打印中性化后的因子值
+print(neutralized_factor_data)
 
