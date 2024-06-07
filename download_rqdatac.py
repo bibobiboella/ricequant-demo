9caa0ff2c29c4d937ef8b8832aa31341ed1dc76d
Ella Yan <81729699+bibobiboella@users.noreply.github.com>
2024-06-06 22:02:57 -0400

    下载2012/1/1-昨天st，停牌，上市不满一年的股票csv本地

--- download_rqdatac.py
+++ download_rqdatac.py
@@ -21,36 +21,32 @@ neutralized_factor_data = pd.DataFrame()
 all_stocks = rqdatac.all_instruments(type='CS')
 all_stock_symbols = all_stocks['order_book_id'].tolist()
 df = rqdatac.get_factor(all_stock_symbols, factor, start_date=start, end_date=end).reset_index()
-#print(df['date'].unique())
 dates = pd.to_datetime(df['date'].unique()).strftime('%Y-%m-%d')
 today = datetime.now().strftime('%Y-%m-%d')
-    #date_str = today.strftime('%Y-%m-%d')
-# 获取每天ST的股票
-st_stocks = rqdatac.is_st_stock(ticker, start_date=start, end_date=end)
-#st_stocks = st_stocks[st_stocks.loc['2024-05-31'] == True]#.index.tolist()
 
+# 获取因子
 csv_file_path = '/Users/ella/factor.csv' 
 df.to_csv(csv_file_path)
 print(f"数据已保存为 {csv_file_path}")
 
+
+# 获取每天ST的股票
+st_stocks = rqdatac.is_st_stock(ticker, start_date=start, end_date=end)
 csv_file_path = '/Users/ella/st_stocks.csv' 
 st_stocks.to_csv(csv_file_path)
 print(f"数据已保存为 {csv_file_path}")
-#df = pd.read_csv(csv_file_path)
 
 
 
-    # 获取每天停牌的股票
+
+# 获取每天停牌的股票
 suspended_stocks = rqdatac.is_suspended(ticker, start_date=start, end_date=end)
-#print(suspended_stocks)
-#suspended_stocks = suspended_stocks[suspended_stocks[end] == True].index.tolist()
 csv_file_path = '/Users/ella/suspended_stocks.csv'
 suspended_stocks.to_csv(csv_file_path)
 print(f"数据已保存为 {csv_file_path}")
 
-#print(suspended_stocks)
 
-    # 获取上市不满一年的股票
+# 获取上市不满一年的股票
 one_year_ago = (datetime.now() - timedelta(days=365)).strftime('%Y-%m-%d')
 newly_listed_stocks = all_stocks[all_stocks['listed_date'] > one_year_ago]['order_book_id'].tolist()
 newly_listed_stocks = pd.DataFrame(newly_listed_stocks)
@@ -58,23 +54,4 @@ csv_file_path = '/Users/ella/newly_listed_stocks.csv'
 newly_listed_stocks.to_csv(csv_file_path)
 print(f"数据已保存为 {csv_file_path}")
 
-"""
-    # 整合所有条件
-filtered_stocks = set(st_stocks.columns) | set(suspended_stocks.columns) | set(newly_listed_stocks)
-
-
-#factor_data = rqdatac.get_factor(ticker, factor, start_date=start, end_date=end)
-#factor_data.loc[all_filtered_stocks, :] = None
-
-print("\n", st_stocks)
-print("\n", suspended_stocks)
-print("\n", newly_listed_stocks)
-print("\n", len(filtered_stocks))
-# 获取因子数据并替换为NA
-#factor_data = get_factor_data()  # 获取因子数据的函数，需要自行定义或根据实际情况调整
-#factor_data.loc[filtered_stocks] = np.nan
-
-# 保存结果
-#factor_data.to_csv('filtered_factor_data.csv')
-"""
-
+#df = pd.read_csv(csv_file_path)
