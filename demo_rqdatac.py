13d26b1dc6c338aa2e06c3c1845a641bf70beae3
Ella Yan <81729699+bibobiboella@users.noreply.github.com>
2024-06-07 02:46:31 -0400

    完成第一步

--- demo_rqdatac.py
+++ demo_rqdatac.py
@@ -31,6 +31,9 @@ print(f"数据已保存为 {csv_file_path}")
 """
 
 
+"""
+#————————————————第一步————————————————
+#找到每天st，停牌，上市不满一年的股票, 并替换为na
 #因子
 csv_file_path_fac = '/Users/ella/factor.csv' 
 #ST的股票
@@ -49,6 +52,10 @@ suspended_stocks = pd.read_csv(csv_file_path_sus)
 newly_listed_stocks_1 = pd.read_csv(csv_file_path_all_1)
 newly_listed_stocks_2 = pd.read_csv(csv_file_path_new_2)
 
+#切成小数据
+#获取一下所有的dates
+dates = factor['date'][-10:]
+#print(dates)
 
 #简简单单清洗一下
 #factor.drop('Unnamed: 0', axis=1)
@@ -71,57 +78,61 @@ newly_listed_stocks_1 = newly_listed_stocks_1[newly_listed_stocks_1['is_one_year
 
 
 #看一下因子index的形式(date, ticker)
-"""
-print("因子: \n", factor.head())
-print("ST股票: \n",st_stocks.head())
-print("停牌: \n", suspended_stocks.head())
-print("上市不满一年_1: \n", newly_listed_stocks_1) #248, 情况1
-print("上市不满一年_2: \n", newly_listed_stocks_2) #179, 情况2
-"""
 
+#print("因子: \n", factor.head())
+#print("ST股票: \n",st_stocks.head())
+#print("停牌: \n", suspended_stocks.head())
+#print("上市不满一年_1: \n", newly_listed_stocks_1) #248, 情况1
+#print("上市不满一年_2: \n", newly_listed_stocks_2) #179, 情况2
+
+
+date = ['2024-05-31']
+for specific_date in date:
 #filter一下所有为True的股票
 #ONLY ticker~
-specific_date = '2024-05-31'
+#specific_date = '2024-05-31'
 #是ST的
-st = st_stocks.loc[specific_date]
-st = st[st == True].index.tolist()
-#print(stocks_st)
+    st = st_stocks.loc[specific_date]
+    st = st[st == True].index.tolist()
+    #print(stocks_st)
 
-#是停牌的
-suspended = suspended_stocks.loc[specific_date]
-suspended = suspended[suspended == True].index.tolist()
-#print(suspended)
+    #是停牌的
+    suspended = suspended_stocks.loc[specific_date]
+    suspended = suspended[suspended == True].index.tolist()
+    #print(suspended)
 
-#是上市不满一年的
-smaller_1yr = newly_listed_stocks_1['order_book_id'].tolist() + newly_listed_stocks_2['order_book_id'].tolist()
-#print(smaller_1yr)
+    #是上市不满一年的
+    smaller_1yr = newly_listed_stocks_1['order_book_id'].tolist() + newly_listed_stocks_2['order_book_id'].tolist()
+    #print(smaller_1yr)
 
 
 
-# 整合所有条件
-filtered_stocks = set(st) | set(suspended) | set(smaller_1yr)
-filtered_stocks = list(filtered_stocks)
-#print("条件三合一: \n  ST+停牌+上市不满一年: \n", filtered_stocks)
+    # 整合所有条件
+    filtered_stocks = set(st) | set(suspended) | set(smaller_1yr)
+    filtered_stocks = list(filtered_stocks)
+    #print("条件三合一: \n  ST+停牌+上市不满一年: \n", filtered_stocks)
 
-#开始替换为Nan
-df_specific_date = factor.loc[specific_date]
-for stock in df_specific_date.index:
-    if stock in filtered_stocks:
-        factor.loc[(specific_date, stock), 'pcf_ratio_total_lyr'] = np.nan
+    #开始替换为Nan
+    mask = (factor.index.get_level_values('date') == specific_date) & (factor.index.get_level_values('order_book_id').isin(filtered_stocks))
+    factor.loc[mask, 'pcf_ratio_total_lyr'] = np.nan
+
+# 打印修改后的 DataFrame
+print("\n修改后的 DataFrame:")
+print(factor)
+
+
+csv_file_path_clean_fac = '/Users/ella/factor_clean.csv'
+factor.to_csv(csv_file_path_clean_fac)
+print(f"数据已保存为 {csv_file_path_clean_fac}")
+
 
 """
-#factor_data = rqdatac.get_factor(ticker, factor, start_date=start, end_date=end)
-#factor_data.loc[all_filtered_stocks, :] = None
 
-print("\n", st_stocks)
-print("\n", suspended_stocks)
-print("\n", newly_listed_stocks_1)
-print("\n", len(filtered_stocks))
-# 获取因子数据并替换为NA
-#factor_data = get_factor_data()  # 获取因子数据的函数，需要自行定义或根据实际情况调整
-#factor_data.loc[filtered_stocks] = np.nan
 
-# 保存结果
-#factor_data.to_csv('filtered_factor_data.csv')
-"""
+#————————————————第二步————————————————
+#第二步，因子去极值和标准化（注意都是在同一个时间截面上，不要在时序上去操作，会导致因子包含有未来信息）
+#读第一步替换好na的数据
+csv_file_path_clean_fac = '/Users/ella/factor_clean.csv'
+df = pd.read_csv(csv_file_path_clean_fac)
+print("第一步替换过na后的表: \n", df.head())
 
